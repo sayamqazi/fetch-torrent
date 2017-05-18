57546c45fdccea398953cf70679bb05430e4cf35
Sayam Qazi <qazisayam@gmail.com>
2017-05-18 13:27:21 +0500

    Separated main loop and functionality
    Now fetch-torrent.py just resolves the command line args and runs the main loop of the script and makes appropriate function calls form other modules.

--- source/fetch-torrent.py
+++ source/fetch-torrent.py
@@ -3,6 +3,7 @@ import os
 import tqdm
 import requests
 import urllib.request
+import Feeders
 
 
 # resolve args
@@ -11,41 +12,15 @@ def resolve_args():
         print("No search keys specified")
         sys.exit(0)
     else:
-        keys = []
+        keys_list = []
         i = 1
         while i < len(sys.argv):
-            keys.append(sys.argv[i])
+            keys_list.append(sys.argv[i])
             i += 1
-        return keys
+        return keys_list
 
-
-# process query
-def build_url(keys):
-    query_string = "+".join(keys)
-    query_url = "https://www.torrentproject.se/?t=" + query_string
-    return query_url
-
-
-# fetch html from url and extract matching torrent_url
-def get_torrent_from_query(query_url):
-    url_req = urllib.request.Request(query_url, headers={"User-Agent": "Magic browser"})
-    with urllib.request.urlopen(url_req) as f:
-        response = f.read()
-    raw = str(response, "utf-8")
-    index = raw.find("<span><a href='/")
-    if index != -1:
-        index += 16
-        torrent_identifier= ""
-        while raw[index] != "/":
-            torrent_identifier += raw[index]
-            index += 1
-        torrent_identifier = torrent_identifier.upper()
-        print("torrent : " + torrent_identifier)
-        prefix = "https://www.torrentproject.se/torrent/"
-        torrent_url = prefix + torrent_identifier + ".torrent"
-        print(torrent_url)
 
 # execution flow of script
-search_keys = resolve_args()
-url = build_url(search_keys)
-get_torrent_from_query(url)
+feeder = Feeders.Feeder()
+keys = resolve_args()
+feeder.from_yts(keys)
